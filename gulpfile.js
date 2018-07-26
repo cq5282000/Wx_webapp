c14763ab28c309b2b273162f9e9da29d68345a67
cq5282000 <18801911877@163.com>
2018-07-26 17:55:23 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
 const gulp = require('gulp');
+const fs = require('fs');
+
+const del = (fileName) => {
+    if (fs.existsSync(fileName)) {
+        fs.readdir(fileName, (fileArr) => {
+            console.log(fileArr);
+        });
+    }
+};
+
+gulp.task('clean', () => {
+    // return del(['dist/*','build/static/images/*'])
+    // fs.
+});
 
 gulp.task('build', () => {
     console.log('build');
 });
 
 gulp.task('default', () => {
-    console.log('default');
+    del('test');
 });
